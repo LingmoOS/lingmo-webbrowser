c11ae8125c5e49a9aaeb6e18655009112861b4af
钦烨 <admibrill@outlook.com>
2025-02-05 11:57:36 +0800

    Update

--- src/components.py
+++ src/components.py
@@ -1,12 +1,18 @@
 from PySide6.QtGui import *
 from PySide6.QtQml import *
 from PySide6.QtQuick import *
+from PySide6.QtCore import*
+from PySide6.QtWebEngineQuick import *
 
-QML_IMPORT_NAME='Components'
-QML_IMPORT_MAJOR_VERSION=1
+class GlobalKeyHandler(QQuickItem):
+    pressed=Signal(QKeyEvent)
+    def __init__(self,parent=None):
+        super().__init__(parent)
+    def eventFilter(self, watched, event):
+        if event.type()==QEvent.Type.KeyPress:
+            print(1)
+            self.pressed.emit(event)
+            return True
+        return False
 
-@QmlElement
-class TabWidget(QQuickItem):
-    def __init__(self):
-        super().__init__()
     
--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
 import sys
-from PySide6.QtGui import *
-from PySide6.QtQml import *
-from PySide6.QtQuick import *
-from PySide6.QtWebEngineQuick import *
+from components import *
 import updRes
 import LingmoUIPy
 import res
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
+    qmlRegisterType(GlobalKeyHandler,'org.lingmo.webbrowser',1,0,'GlobalKeyHandler')
     QtWebEngineQuick.initialize()
     engine = QQmlApplicationEngine()
     engine.load('qrc:/qml/main.qml')
     if not engine.rootObjects():
         sys.exit(-1)
+    mainRootObject=engine.rootObjects()[0].children()[0]
+    globalKeyHandler=mainRootObject.findChild(QObject,"global_key_handler")
+    mainRootObject.installEventFilter(globalKeyHandler)
     sys.exit(app.exec())
